0878b54e3933c0fe26f2b71210e8c2b1f5628c68
ThangaDhiwanV <vthangadhiwan02@gmail.com>
2025-05-10 02:33:26 +0530

    Fix default export in InstrumentGrid component

--- src/components/InstrumentCard/InstrumentGrid.tsx
+++ src/components/InstrumentCard/InstrumentGrid.tsx
@@ -1,1 +1,18 @@
-// Content from the file changes shown above
+import React from 'react';
+import { InstrumentCard } from './InstrumentCard';
+
+interface InstrumentGridProps {
+  instruments: any[];
+}
+
+const InstrumentGrid: React.FC<InstrumentGridProps> = ({ instruments }) => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      {instruments.map((instrument) => (
+        <InstrumentCard key={instrument.id} instrument={instrument} />
+      ))}
+    </div>
+  );
+};
+
+export default InstrumentGrid;
